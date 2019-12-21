8c0de1924538b0bd7f7b7c8b30b6cda4cf5bccce
Ayrat <56065447+Kran2288@users.noreply.github.com>
2019-12-21 16:08:54 +0300

    1Save

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    alert('Структура страницы сформирована, можно приступать!');
+});
+
+
+$('h1').css({
+    'color': 'red'
+});
+
+
+$(".content").addClass("necontent");
+$('.necontent').css({
+    'color': 'green'
+});
+
+$('#way').click(function () {
+    alert('Вы нажали на элемент "way"');
+});
+
+$("div").find("span");
+
+
+$('li').filter(function (index) {
+    return $('strong', this).length == 1;
+}).css('background-color', 'red');
